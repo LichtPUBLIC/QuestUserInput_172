7204a994be5d4865d1702bcbc1feea1c803d3ee4
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-10-31 10:24:51 +0700

    Tambahkan OutlinedTextField untuk input Nama

--- app/src/main/java/com/example/prak4/TeksEdit.kt
+++ app/src/main/java/com/example/prak4/TeksEdit.kt
@@ -41,12 +41,21 @@ fun FormDataDiri(modifier: Modifier
     var alamat by remember { mutableStateOf("") }
     var jenis by remember { mutableStateOf("") }
 
-    val gender: List<String> = listOf("Laki-Laki", "Perempuan")
+    val gender:List<String> = listOf("Laki-Laki", "Perempuan")
 
-    Column(
-        modifier = Modifier.padding(top = 50.dp),
+    Column(modifier = Modifier.padding(top = 50.dp),
         verticalArrangement = Arrangement.Top,
 
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {}
-}
+    ) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
+    }}
