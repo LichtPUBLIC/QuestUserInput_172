585f11efde6d692431fda5c821f3074fbeea86d7
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-02 02:44:24 +0700

    Setup kerangka dasar composable QuestUserInput

---
+++ app/src/main/java/com/example/prak4/QuestUserInput.kt
@@ -0,0 +1,13 @@
+package com.example.prak4
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Arrangement
+
+@Composable
+fun QuestUserInput(modifier: Modifier = Modifier) {
+
+}
