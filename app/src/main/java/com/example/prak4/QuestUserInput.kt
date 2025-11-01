65682ca321dfea9f264a66c13caea71d46b6a8fb
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-02 02:55:45 +0700

    Menambahkan teks judul formulir ("Formulir Pendaftaran").

--- app/src/main/java/com/example/prak4/QuestUserInput.kt
+++ app/src/main/java/com/example/prak4/QuestUserInput.kt
@@ -1,32 +1,39 @@
 package com.example.prak4
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.unit.dp
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.foundation.layout.fillMaxWidth
 
 val PrimaryColor = Color(0xFF5C6BC0)
 val darkTextColor = Color.Black
 val lightPlaceholderColor = Color.Gray
-
 @Composable
 fun QuestUserInput(modifier: Modifier = Modifier) {
     var namaLengkap by remember { mutableStateOf("") }
@@ -46,21 +53,13 @@ fun QuestUserInput(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start
     ) {
+
         Text(
-            text = "NAMA LENGKAP",
-            fontSize = 12.sp,
+            text = "Formulir Pendaftaran",
+            fontSize = 22.sp,
             fontWeight = FontWeight.Bold,
             color = darkTextColor,
-            modifier = Modifier.padding(top = 70.dp, bottom = 8.dp) // Penyesuaian padding awal
-        )
-
-        OutlinedTextField(
-            value = namaLengkap,
-            onValueChange = { namaLengkap = it },
-            placeholder = { Text("Masukan nama lengkap", color = lightPlaceholderColor) },
-            modifier = Modifier.fillMaxWidth(),
-            singleLine = true,
-            shape = MaterialTheme.shapes.small
+            modifier = Modifier.padding(bottom = 24.dp)
         )
     }
 }
