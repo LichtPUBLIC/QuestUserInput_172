32063a8993e3a839e902cd21ed97666a4bcf3ba4
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-02 02:44:45 +0700

    Definisikan state input nama, JK, status, dan alamat

--- app/src/main/java/com/example/prak4/QuestUserInput.kt
+++ app/src/main/java/com/example/prak4/QuestUserInput.kt
@@ -6,8 +6,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun QuestUserInput(modifier: Modifier = Modifier) {
+    var namaLengkap by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var statusPerkawinan by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+
 
 }
